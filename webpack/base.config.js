3ff167855f6897570c62b053e78423993a20a30f
Mike Harris <hello@mikemjharris.com>
2018-12-17 16:38:42 +0000

    webpack setup

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -3,34 +3,48 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  context: path.resolve(__dirname, '../src'),
+
   entry: {
-		entry: './src/index.js',
+    entry: './index.js',
   },
 
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, '../dist')
-	},
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
 
-
-	module: {
-		rules: [{
-			test: /\.scss$/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				{
-					loader: "css-loader"
-				}, {
-					loader: "sass-loader"
-				}]
-		}]
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "./style.css"
-		}),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./style.css"
+    }),
     new webpack.EnvironmentPlugin([
-      'NODE_ENV',
+        'NODE_ENV',
     ]),
   ],
 };
